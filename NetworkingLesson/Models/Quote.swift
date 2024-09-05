7f9d0a0848650369b33352f85bb73ca24f81e7d8
Alex <alex@polyform.co>
2024-09-05 19:54:44 +0200

    first api

---
+++ NetworkingLesson/Models/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  NetworkingLesson
+//
+//  Created by Alex Pesenka on 05/09/24.
+//
+
+import Foundation
+
+struct Quote: Decodable {
+    let q: String
+    let a: String
+}
